b58bf885401af6e579ea730ad0b28d0bf063b337
Alexey <boronin_1993@mail.ru>
2023-12-15 19:43:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Метод Parallel.Invoke выполняет три метода
+Parallel.Invoke(
+    Print,
+    () =>
+    {
+        Console.WriteLine($"Выполнение задач {Task.CurrentId}");
+        Thread.Sleep(3000);
+    },
+    () => Square(7)
+    );
+void Print()
+{
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+    Thread.Sleep(3000);
+}
+void Square(int n)
+{
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+    Thread.Sleep(3000);
+    Console.WriteLine($"Результат {n * n}");
+}
+Parallel.For(1, 5, Square);
+ParallelLoopResult result = Parallel.ForEach<int>(
+    new List<int>() { 1, 3, 5, 7, 9, 11 },
+    Square);
+ParallelLoopResult result1 = Parallel.For(1, 10, Square1);
+if (!result1.IsCompleted)
+    Console.WriteLine($"Выполнение цикла завершено на итерации {result.LowestBreakIteration}");
+void Square1(int n, ParallelLoopState pls)
+{
+    if (n == 6) pls.Break();
+    Console.WriteLine($"Квадрат числа {n} равен {n * n}");
+    Thread.Sleep(1000);
+}
